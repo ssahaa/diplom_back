d396b88a68d3ebbc24d67fa3330365d718625a8e
ssahaa <9654110677@mail.ru>
2024-04-25 20:48:25 +0300

    create PIPENV

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-123
+import sys
+from PyQt6 import QtWidgets
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.button_is_checked = True
+
+        self.setWindowTitle("My App")
+
+        self.button = QtWidgets.QPushButton("Press Me!")
+        self.button.setCheckable(True)
+        self.button.released.connect(self.the_button_was_released)
+        self.button.setChecked(self.button_is_checked)
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_released(self):
+        self.button_is_checked = self.button.isChecked()
+
+        print(self.button_is_checked)
+
+
+app = QtWidgets.QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
