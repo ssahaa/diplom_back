128f97ecff0c9a09949910e60c8e44676b268394
ssahaa <9654110677@mail.ru>
2024-05-12 15:31:16 +0300

    1

--- main.py
+++ main.py
@@ -1,30 +1,9 @@
-import sys
-from PyQt6 import QtWidgets
-
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.button_is_checked = True
-
-        self.setWindowTitle("My App")
-
-        self.button = QtWidgets.QPushButton("Press Me!")
-        self.button.setCheckable(True)
-        self.button.released.connect(self.the_button_was_released)
-        self.button.setChecked(self.button_is_checked)
-
-        self.setCentralWidget(self.button)
-
-    def the_button_was_released(self):
-        self.button_is_checked = self.button.isChecked()
-
-        print(self.button_is_checked)
-
-
-app = QtWidgets.QApplication(sys.argv)
-
-window = MainWindow()
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QPushButton('Top'))
+layout.addWidget(QPushButton('Bottom'))
+window.setLayout(layout)
 window.show()
-
-app.exec()
+app.exec_() 
